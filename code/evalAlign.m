ef3ffbae8f13ed69fcc62cf6ef39de92430555a5
yellowsn0w <dmarch1993@gmail.com>
2016-03-11 16:22:18 -0500

    work on p4

--- code/evalAlign.m
+++ code/evalAlign.m
@@ -20,36 +20,37 @@ LMF = lm_train( trainDir, 'f', fn_LMF );
 
 % Train your alignment model of French, given English 
 num_runs = [1000, 5000, 10000, 30000];
-bleu_param = [1,2,3]
-%AMs = struct()
+bleu_param = [1,2,3];
 
 for index=1:length(num_runs)
     AM = align_ibm1( trainDir, num_runs{index});
-% ... TODO: more 
-
-% TODO: a bit more work to grab the English and French sentences. 
-%       You can probably reuse your previous code for this  
+    
+    % hansard references
     DF = dir( '/u/cs401/A2_SMT/data/Hansard/Testing/Task5.f' );
     DE = dir( '/u/cs401/A2_SMT/data/Hansard/Testing/Task5.e' );
     f_lines = textread(DF, '%s','delimiter','\n');
     e_lines = textread(DE, '%s','delimiter','\n');
     
+    % google references
+    GOOGLE = dir( '/u/cs401/A2_SMT/data/Hansard/Testing/Task5.e' );
+    e_lines_google = textread(GOOGLE, '%s','delimiter','\n');
+    
+    % IBM BlueMix references
+    [status, cmdout] = sprintf('curl -u "{username}":"{password}" -X POST -F "text=%s"-F "source=fr" -F "target=en" "https://gateway.watsonplatform.net/language-translation/api/v2/translate"', f_lines{index});
+    
     for sentence=1:length(f_lines)
         for n=1:length(bleu_param)
-            eng = decode( f_lines{sentence}, LME, AM);
-            actual = e_lines{sentence};
-            bleu_score = 
-    
-
-
-% TODO: perform some analysis
-% add BlueMix code here 
-username = '""';
-password = '""';
-url = '""';
-
-for 1:
-    curl
-
-
-[status, result] = unix('')
+            eng = decode(f_lines{sentence}, LME, AM);
+            
+            % 3 references
+            actual_hansard = e_lines{sentence};
+            actual_google = e_lines_googles{sentences};
+            actual_ibm = cmdout;
+            
+            % calculate bleu score
+            bleu_score1 = bleu_score();
+            bleu_score2 = bleu_score();
+            bleu_score3 = bleu_score();
+        end
+    end
+end
