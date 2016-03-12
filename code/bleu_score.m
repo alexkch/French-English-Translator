d5b01a95c1be4ae95e29d859a8a8fb4195fcab77
Chang Kao Tsun <c2changk@cdf.toronto.edu>
2016-03-12 18:44:16 -0500

    finishe dup last parts

--- code/bleu_score.m
+++ code/bleu_score.m
@@ -1,35 +1,60 @@
+
 function bleu_score = bleu_score(candidate, references, n)
+%
+%
+%  This function returns a bleu_score based on the candidate sentence
+%  and reference sentences and calculates it from this. See lecture 6-1 for more details
+%
+%  INPUTS:
+%
+%       candidate    	: (string) a preprocessed sentence
+%       references      : (array of strings) reference sentences for the candidate      
+%       n        	: 1 <= n <= 3   
+%
+% 
+% By Alex Chang (Kao-Tsun Chang)     
 
     candidate_words = strsplit(' ', candidate);
     
-    candidate_length = length(candidate_words) - 2; % Subtract SENTSTART & SENTEND
+    candidate_length = length(candidate_words); 
     
+    %data count for 
     blue_score = struct();
     blue_score.unigram_count = 0;
     blue_score.bigram_count = 0;
     blue_score.trigram_count = 0;
     
+    % for tracking words that have already appeared in at least one reference sentence
     marked_words = struct();
     
-    for i=2:length(candidate_words)-1 %to skip start and end in candidate words
+    for i=1:length(candidate_words) % The sentence does not start with SENTSTART nor ends with SENTEND, we dont have to account for it
         
         for sentence_i=1:length(references) 
             
             ref_words = strsplit(' ', references{sentence_i});
             
-            for j=2:length(ref_words)-1 %Assume the reference sentences start with SENTSTART and SENTEND
-            
+            for j=1:length(ref_words)  % The sentence does not start with SENTSTART nor ends with SENTEND, we dont have to account for it
+                
+                % if candidate word is in the ref sentence, increment uni
+                % count
                 if strcmpi(candidate_words{i}, ref_words{j}) 
+                    % mark it in marked_words as already counted
                     if isfield(marked_words, ref_words{j}) == 0 
                         blue_score.unigram_count = blue_score.unigram_count + 1;
                         marked_words.(candidate_words{i}) = struct();
                     end
-                    if (i < length(candidate_words) - 1) && (j < length(ref_words) - 1) && (strcmpi(candidate_words{i+1}, ref_words{j+1}))                    
+                    %if candidate word k and k+1 are both in reference
+                    %sentence, increment bi
+                    if (i < length(candidate_words)) && (j < length(ref_words)) && (strcmpi(candidate_words{i+1}, ref_words{j+1}))                    
+                        % mark k.k+1 as a field in marked_words
                         if isfield(marked_words.(candidate_words{i}), ref_words{j+1}) == 0 
                             blue_score.bigram_count = blue_score.bigram_count + 1;
                             marked_words.(candidate_words{i}).(candidate_words{i+1}) = struct();
                         end
-                        if (i < length(candidate_words) - 2) && (j < length(ref_words) - 2) && (strcmpi(candidate_words{i+2}, ref_words{j+2}))
+                        %if candidate word k, k+1 and k+2 are both in reference
+                        %sentence, increment tri
+                        if (i < length(candidate_words) - 1) && (j < length(ref_words) - 1) && (strcmpi(candidate_words{i+2}, ref_words{j+2}))
+                            % mark k.k+1.k+2 as a field in marked words
                             if isfield(marked_words.(candidate_words{i}), ref_words{j+1}) == 0 
                                 blue_score.trigram_count = blue_score.trigram_count + 1;
                                 marked_words.(candidate_words{i}).(candidate_words{i+1}).(candidate_words{i+1}) = 1;
@@ -41,35 +66,25 @@ function bleu_score = bleu_score(candidate, references, n)
         end
     end
     
-        disp('uni');
-        disp(blue_score.unigram_count);
-        disp('bi');
-        disp(blue_score.bigram_count);
-        disp('tri');
-        disp(blue_score.trigram_count);
     
+    % the different options for n
     if n == 1
-        uni_prob = blue_score.unigram_count / (candidate_length); %dont count SENTSTART/SENTEND
+        uni_prob = blue_score.unigram_count / (candidate_length); 
         bi_prob = 1;
         tri_prob = 1;
-        disp('uni');
-        disp(uni_prob);
+
     end
     if n == 2
         uni_prob = blue_score.unigram_count / (candidate_length);
-        bi_prob = blue_score.bigram_count /  (candidate_length - 1); %dont count SENTSTART/SENTEND
+        bi_prob = blue_score.bigram_count /  (candidate_length - 1); 
         tri_prob = 1;
-        disp('uni');
-        disp(uni_prob);
-        disp('bi');
-        disp(bi_prob);
+
     end
     if n == 3
         uni_prob = blue_score.unigram_count / (candidate_length);
         bi_prob = blue_score.bigram_count /  (candidate_length - 1);
-        tri_prob = blue_score.trigram_count / (candidate_length - 2); %dont count SENTSTART/SENTEND
-        disp('tri');
-        disp(tri_prob);
+        tri_prob = blue_score.trigram_count / (candidate_length - 2); 
+
     end
     
     BPc = penalty(candidate_length, references);  
@@ -78,11 +93,25 @@ function bleu_score = bleu_score(candidate, references, n)
 end
 
 function BPc = penalty(candidate_length, references)
-    
+%
+%
+%  This function calculates the penalty BPc from candidate length and references
+%
+%  INPUTS:
+%
+%       candidate_length	: length of candidate sentence
+%       references      	: (array of strings) reference sentences for the candidate      
+%
+% 
+% By Alex Chang (Kao-Tsun Chang)     
+
+    %set smallest to Inf
     smallest = Inf;
+    
+    %set closest length to 0
     closest_length = 0;
     for i=1:length(references)
-        ref_length = length(strsplit(' ', references{i})) - 2; % Subtract START/ENDAssume the reference sentences do not start with SENTSTART and SENTEND
+        ref_length = length(strsplit(' ', references{i})); % ref sentences do not start with SENTSTART and SENTEND
         difference = candidate_length - ref_length;
         if abs(difference) < smallest
             smallest = difference;
@@ -93,6 +122,5 @@ function BPc = penalty(candidate_length, references)
     brevity = closest_length/ candidate_length; % brev = ref/can
     
     BPc = exp(1 - brevity); % if rev == can, BPc is 0, otherwise it is e^(1-brev)
-    disp('BPc');
-    disp(BPc);
+
 end
--- code/evalAlign.m
+++ code/evalAlign.m
@@ -2,46 +2,58 @@ function evalAlign()
 %
 % evalAlign
 %
-%  This is simply the script (not the function) that you use to perform your evaluations in 
-%  Task 5. 
+% Templete file to get output, using all the parts from part 1 through 4
+%
 
+%output file
+fileOUT_ID = fopen('/h/u4/c2/06/c2changk/csc401/CSC401-a2/code/data/outFILE','w');
+    
+%credentials
 username = '"ab8ed0c1-c0ab-4d60-98f6-55ef0a8e39cc"';
 password = '"Owzp3kcvdRLG"';
 url = '"https://gateway.watsonplatform.net/language-translation/api/v2/translate"';
 
+%variables
 trainDir     = '/u/cs401/A2_SMT/data/Hansard/Training';
 testDir      = '/u/cs401/A2_SMT/data/Hansard/Testing';
 fn_LME       = 'out_LME';
+fn_AM        = 'out_AM';
 lm_type      = '';
 delta        = 0;
 maxIter      = 10;
 
+% references
 LinesFr = textread('/u/cs401/A2_SMT/data/Hansard/Testing/Task5.f', '%s', 'delimiter', '\n');
 refE = textread('/u/cs401/A2_SMT/data/Hansard/Testing/Task5.e', '%s', 'delimiter', '\n');
 refGoogle = textread('/u/cs401/A2_SMT/data/Hansard/Testing/Task5.google.e', '%s', 'delimiter', '\n');
-sen_len=[1000, 10000, 15000, 30000];
+
+% parameters
+sen_len= [1000]; %test_len
+%sen_len=[1000, 10000, 15000, 30000]; %
 bleu_param = [1,2,3];
 
+% Init our AMs
 AMs = struct();
-% Train your alignment model of French, given English 
 
 for a=1:length(sen_len)
-    AMs.(['AM' num2str(sen_len(a))]) = align_ibm1(trainDir, sen_len(a), maxIter, f);
+    AMs.(['AM' num2str(sen_len(a))]) = align_ibm1(trainDir, sen_len(a), maxIter, fn_AM);
 end
+%AMs.(['AM' num2str(sen_len(1))]) = importdata('/h/u4/c2/06/c2changk/csc401/CSC401-a2/code/real10k');
 
 % Train your language models. This is task 2 which makes use of task 1
-LME = lm_train(trainDir, 'e', fn_LME );
-vocabSize = length(fieldnames(LMs.LME.uni));
+%LME = lm_train(trainDir, 'e', fn_LME );
+LME = importdata('/h/u4/c2/06/c2changk/csc401/CSC401-a2/code/data/Eng_LM');
+vocabSize = length(fieldnames(LME.uni));
 
+
+
+% CALCULATION BLUESCORE
+
+fprintf(fileOUT_ID, '::::::::::::::::::::::::::::::::::OUTPUT:::::::::::::::::::::::::::::::::'); 
 for lines=1:length(LinesFr)
     
-    curl = ['curl -u ' username ':' password ' -X POST -F "text=' LinesFr{lines} '" -F "source=fr" -F "target=en" ' url];
+    curl = ['env LD_LIBRARY_PATH='''' curl -u ' username ':' password ' -X POST -F "text=' LinesFr{lines} '" -F "source=fr" -F "target=en" ' url];
     [status, refbluemix] = unix(curl); 
-    
-    disp('bluemix status [status, reference]');
-    disp(status);
-    disp(refbluemix);
-    
     
     ref1 = preprocess(refE{lines}, 'e');
     ref1 = removeSENT(ref1);
@@ -51,33 +63,42 @@ for lines=1:length(LinesFr)
     ref3 = removeSENT(ref3);
     
     references = {ref1, ref2, ref3};
-    disp('references 1. E_lines Trans 2. Google Trans 3. Bluemix Trans')
-    disp(references);
+    fprintf(fileOUT_ID, '\r\n\r\n_______________________________________________________\r\n'); 
+    fprintf(fileOUT_ID, ':::REFERENCES::: \r\n1. E_lines Trans 2. Google Trans 3. Bluemix Trans\r\n');
+    for c=1:length(references)
+        fprintf(fileOUT_ID, references{c});
+        fprintf(fileOUT_ID, '\r\n');
+    end
     
     processedFr = preprocess(LinesFr{lines}, 'f');
+    processedFr = removeSENT(processedFr);
     
     for num_sent=1:length(sen_len)
-        
+
         candidate = decode2(processedFr, LME, AMs.(['AM' num2str(sen_len(num_sent))]), lm_type, delta, vocabSize);
-        
-        disp('candidate & references')
-        disp(candidate);       
-        
+        fprintf(fileOUT_ID, '\r\nAM sentence_length: %d\r\n\r\n', sen_len(num_sent));    
         for num=1:length(bleu_param)
             n = bleu_param(num);
-            bleu_score = bleu_score(candidate, references, n);
-            disp('bleu_score @ n');
-            disp(n);
-            disp(bleu_score);
+            bleu = bleu_score(candidate, references, n);
+            fprintf(fileOUT_ID, 'CANDIDATE SENTENCE:  \r\n');
+            fprintf(fileOUT_ID, candidate);
+            fprintf(fileOUT_ID, '\r\n\r\n');
+            fprintf(fileOUT_ID, 'bleu_score @ %d N: %4.7f \r\n', n, bleu);
+            fprintf(fileOUT_ID, '\r\n');
+            fprintf(fileOUT_ID, '\r\n');            
         end
     end
 end
+end
 
+
+% Removes SENTSTART and SENTEND from sentence
 function out = removeSENT(sentence)
     
     out = regexprep(sentence, 'SENTSTART\s*', '');
     out = regexprep(out, 'SENTEND\s*', '');
 
 end
+
 
 
